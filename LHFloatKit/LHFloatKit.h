674397b9b53fed00f8c8aee9e701f3491d1915cb
Li-Heng Hsu <yesleon@me.com>
2018-11-15 14:38:00 +0800

    Initial Commit

---
+++ LHFloatKit/LHFloatKit.h
@@ -0,0 +1,19 @@
+//
+//  LHFloatKit.h
+//  LHFloatKit
+//
+//  Created by 許立衡 on 2018/11/15.
+//  Copyright © 2018 narrativesaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LHFloatKit.
+FOUNDATION_EXPORT double LHFloatKitVersionNumber;
+
+//! Project version string for LHFloatKit.
+FOUNDATION_EXPORT const unsigned char LHFloatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LHFloatKit/PublicHeader.h>
+
+
